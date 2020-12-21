107f6ad86105c72630e6e47545e92287e0af18dd
Alice Lieutier <alice.lieutier@gmail.com>
2020-12-21 12:54:24 +0000

    add socket.io on server side

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 
 const app = express()
 const port = process.env.PORT || 8000
@@ -20,7 +22,15 @@ app.get("/game/:id", (req: Request, res: Response) => {
     const id = req.params.id
     res.render('game', {id});
 })
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
+
+io.on("connection", (socket: Socket) => {
+  console.log("connection made")
+  console.log(socket)
+});
   
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Server Started at port: ${port}`)
 })
