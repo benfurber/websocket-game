f00d84c0db879bfff921e19aaf4bca2e518df3e6
benfurber <ben@fu.rber.io>
2020-12-21 11:59:35 +0000

    ✨ Add create game route

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,19 @@ app.use(cors());
 app.set('view engine', 'ejs');
 
 app.get("/", (req: Request, res: Response) => {
-  res.render('index', {});
+    res.render('index', {});
 })
 
+app.post("/game", (req: Request, res: Response) => {
+  const id = "333"
+  res.redirect(`/game/${id}`)
+})
+
+app.get("/game/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+    res.render('game', {id});
+})
+  
 app.listen(8000,()=>{
   console.log('Server Started at Port, 8000')
 })
